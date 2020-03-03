f4f9445d3d9e5d6a1d7faa20c92fcdf6eefc339f
Benjamin Mønster <Benjamin.Moenster@gmail.com>
2020-03-03 08:35:12 +0100

    upDate

---
+++ Afleveringsopgaven/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Afleveringsopgaven
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            idBox.SetBinding(TextBox.TextProperty, new Binding(nameof(User.ID)));
+            nameBox.SetBinding(TextBox.TextProperty, new Binding(nameof(User.Name)));
+            ageBox.SetBinding(TextBox.TextProperty, new Binding(nameof(User.Age)));
+            scoreBox.SetBinding(TextBox.TextProperty, new Binding(nameof(User.Score)));
+
+        }
+
+        private void MenuItem_Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+
+                using (var file = new StreamReader(filename))
+                {
+                    string line;
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        User p = new User(line);
+                        UserList.Items.Add(p);
+                    }
+                }
+                ListCount.Content = UserList.Items.Count;
+                LastUpdate.Content = $"Last update: {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}";
+            }
+        }
+        
+        private void PeopleList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (UserList.SelectedItem is User)
+            {
+                idBox.DataContext = UserList.SelectedItem;
+                nameBox.DataContext = UserList.SelectedItem;
+                ageBox.DataContext = UserList.SelectedItem;
+                scoreBox.DataContext = UserList.SelectedItem;
+            }
+        }
+    }
+}
---
+++ Afleveringsopgaven/User.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+
+namespace Afleveringsopgaven
+{
+    class User
+    {
+        private int id;
+        private String name;
+        private int age;
+        private int score;
+
+        public int ID
+        {   
+            set { id = value; }
+            get { return id; }
+        }
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public int Age
+        {
+            set { age = value; }
+            get { return age; }
+        }
+
+        public int Score
+        {
+            set { score = value; }
+            get { return score; }
+        }
+
+
+        public User(string person)
+        {
+            string[] s = person.Split(';');
+            this.id = int.Parse(s[0]);
+            this.name = s[1];
+            this.age = int.Parse(s[2]);
+            this.score = int.Parse(s[3]);
+        }
+
+        override public string ToString()
+        {
+            return String.Format("{0, -5} {1, -4}", id, name);
+        }
+    }
+}
